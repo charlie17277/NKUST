3aefcc82bc2914acf03e7060a7a533b448ae84c3
charlie17277 <93572308+charlie17277@users.noreply.github.com>
2021-11-09 16:36:06 +0800

    Add files via upload

---
+++ BMI.java
@@ -0,0 +1,7 @@
+public class BMI {
+    public static void main(String[] args) {
+        int a = 154;
+        int b = 39;
+        System.out.println(b/((a/100)^2));
+    }
+}
---
+++ Variable.java
@@ -0,0 +1,6 @@
+public class Variable {
+    public static void main(String[] args) {
+        String myName = "Charlie Yan";
+        System.out.println("Hello " + myName);
+    }
+}
